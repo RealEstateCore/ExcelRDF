aa0d5a661af084e1a6341dbb4f2c578e5607943f
Karl Hammar <karl@karlhammar.com>
2019-02-25 14:30:15 +0100

    Initial commit.

---
+++ RdfTranslationAddIn/Ribbon.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace RdfTranslationAddIn
+{
+    partial class Ribbon : Microsoft.Office.Tools.Ribbon.RibbonBase
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        public Ribbon()
+            : base(Globals.Factory.GetRibbonFactory())
+        {
+            InitializeComponent();
+        }
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tab1 = this.Factory.CreateRibbonTab();
+            this.RdfTranslationGroup = this.Factory.CreateRibbonGroup();
+            this.loadOntologyButton = this.Factory.CreateRibbonButton();
+            this.exportRdfButton = this.Factory.CreateRibbonButton();
+            this.tab1.SuspendLayout();
+            this.RdfTranslationGroup.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tab1
+            // 
+            this.tab1.ControlId.ControlIdType = Microsoft.Office.Tools.Ribbon.RibbonControlIdType.Office;
+            this.tab1.ControlId.OfficeId = "TabData";
+            this.tab1.Groups.Add(this.RdfTranslationGroup);
+            this.tab1.Label = "TabData";
+            this.tab1.Name = "tab1";
+            // 
+            // RdfTranslationGroup
+            // 
+            this.RdfTranslationGroup.Items.Add(this.loadOntologyButton);
+            this.RdfTranslationGroup.Items.Add(this.exportRdfButton);
+            this.RdfTranslationGroup.Label = "RDF Translation";
+            this.RdfTranslationGroup.Name = "RdfTranslationGroup";
+            this.RdfTranslationGroup.Position = this.Factory.RibbonPosition.AfterOfficeId("GroupGetExternalData");
+            // 
+            // loadOntologyButton
+            // 
+            this.loadOntologyButton.Label = "Load Ontology";
+            this.loadOntologyButton.Name = "loadOntologyButton";
+            this.loadOntologyButton.ShowImage = true;
+            this.loadOntologyButton.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.loadOntologyButton_Click);
+            // 
+            // exportRdfButton
+            // 
+            this.exportRdfButton.Label = "Export RDF";
+            this.exportRdfButton.Name = "exportRdfButton";
+            this.exportRdfButton.ShowImage = true;
+            this.exportRdfButton.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.exportRdfButton_Click);
+            // 
+            // Ribbon
+            // 
+            this.Name = "Ribbon";
+            this.RibbonType = "Microsoft.Excel.Workbook";
+            this.Tabs.Add(this.tab1);
+            this.Load += new Microsoft.Office.Tools.Ribbon.RibbonUIEventHandler(this.Ribbon_Load);
+            this.tab1.ResumeLayout(false);
+            this.tab1.PerformLayout();
+            this.RdfTranslationGroup.ResumeLayout(false);
+            this.RdfTranslationGroup.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        internal Microsoft.Office.Tools.Ribbon.RibbonTab tab1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup RdfTranslationGroup;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton loadOntologyButton;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton exportRdfButton;
+    }
+
+    partial class ThisRibbonCollection
+    {
+        internal Ribbon Ribbon
+        {
+            get { return this.GetRibbon<Ribbon>(); }
+        }
+    }
+}
---
+++ RdfTranslationAddIn/Ribbon.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Windows.Forms;
+using Microsoft.Office.Tools.Ribbon;
+using VDS.RDF.Ontology;
+using VDS.RDF.Parsing;
+using VDS.RDF;
+
+namespace RdfTranslationAddIn
+{
+    public partial class Ribbon
+    {
+        private void Ribbon_Load(object sender, RibbonUIEventArgs e)
+        {
+
+        }
+
+        private string GetExcelColumnName(int columnNumber)
+        {
+            int dividend = columnNumber;
+            string columnName = String.Empty;
+            int modulo;
+
+            while (dividend > 0)
+            {
+                modulo = (dividend - 1) % 26;
+                columnName = Convert.ToChar(65 + modulo).ToString() + columnName;
+                dividend = (int)((dividend - modulo) / 26);
+            }
+
+            return columnName;
+        }
+
+        private void loadOntologyButton_Click(object sender, RibbonControlEventArgs e)
+        {
+            // Displays an OpenFileDialog so the user can select an ontology.  
+            OpenFileDialog openOntologyFileDialog = new OpenFileDialog();
+            openOntologyFileDialog.Filter = "OWL Ontologies|*.owl";
+            openOntologyFileDialog.Title = "Select an OWL ontology file";
+
+            // Show the Dialog.  
+            // If the user clicked OK in the dialog and an OWL file was selected, open it.  
+            if (openOntologyFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                OntologyGraph g = new OntologyGraph();
+                FileLoader.Load(g, openOntologyFileDialog.FileName);
+
+                // Iterate through the named bottom classes; generate one worksheet for each (named from URI fragment)
+                foreach (OntologyClass oClass in g.OwlClasses)
+                {
+                    if (oClass.IsBottomClass && oClass.Resource.NodeType == NodeType.Uri)
+                    {
+                        Excel.Worksheet newWorksheet = Globals.ThisAddIn.Application.Worksheets.Add();
+                        UriNode classAsUriNode = (UriNode)oClass.Resource;
+                        string classFragment = classAsUriNode.Uri.Fragment.TrimStart('#');
+                        newWorksheet.Name = classFragment;
+
+                        // Iterate through the properties for which this class is in the domain; 
+                        // generate one column for each property (named from IRI fragment)
+                        int column = 1;
+                        foreach (OntologyProperty oProperty in oClass.IsDomainOf)
+                        {
+                            if (oProperty.Resource.NodeType == NodeType.Uri)
+                            {
+                                // This is because Excel uses strange adressing, i.e., "A1" instead of something 
+                                // numeric and zero-indexed such as "0,0".
+                                string headerColumnName = GetExcelColumnName(column);
+                                string headerCellIdentifier = String.Format("{0}1", headerColumnName);
+                                Excel.Range headerCellRange = newWorksheet.get_Range(headerCellIdentifier);
+
+                                UriNode propertyAsUriNode = (UriNode)oProperty.Resource;
+                                string propertyFragment = propertyAsUriNode.Uri.Fragment.TrimStart('#');
+                                headerCellRange.Value = propertyFragment;
+                                column++;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private void exportRdfButton_Click(object sender, RibbonControlEventArgs e)
+        {
+
+        }
+    }
+}
