2a126bb83ab0ca95ecd071a71f62f360c1b084cc
Karl Hammar <karl@karlhammar.com>
2019-04-23 10:51:23 +0200

    Dunno?

--- RdfTranslationAddIn/ExportOptionsForm.Designer.cs
+++ RdfTranslationAddIn/ExportOptionsForm.Designer.cs
@@ -29,6 +29,8 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
             this.okButton = new System.Windows.Forms.Button();
             this.cancelButton = new System.Windows.Forms.Button();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
@@ -37,14 +39,14 @@
             this.exportNamespaceLabel = new System.Windows.Forms.Label();
             this.removePrefixButton = new System.Windows.Forms.Button();
             this.addPrefixButton = new System.Windows.Forms.Button();
-            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
-            this.namespacePrefixesView = new System.Windows.Forms.DataGridView();
             this.addDataNamespaceToMappingsButton = new System.Windows.Forms.Button();
+            this.namespacePrefixesView = new System.Windows.Forms.DataGridView();
             this.prefix = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.nspace = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
             this.tableLayoutPanel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.namespacePrefixesView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
             this.SuspendLayout();
             // 
             // okButton
@@ -164,33 +166,6 @@
             this.addPrefixButton.UseVisualStyleBackColor = true;
             this.addPrefixButton.Click += new System.EventHandler(this.addPrefixButton_Click);
             // 
-            // errorProvider1
-            // 
-            this.errorProvider1.ContainerControl = this;
-            // 
-            // namespacePrefixesView
-            // 
-            this.namespacePrefixesView.AllowUserToAddRows = false;
-            this.namespacePrefixesView.AllowUserToDeleteRows = false;
-            this.namespacePrefixesView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.namespacePrefixesView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
-            this.namespacePrefixesView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.namespacePrefixesView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.prefix,
-            this.nspace});
-            this.namespacePrefixesView.Location = new System.Drawing.Point(172, 80);
-            this.namespacePrefixesView.MultiSelect = false;
-            this.namespacePrefixesView.Name = "namespacePrefixesView";
-            this.namespacePrefixesView.RowHeadersVisible = false;
-            this.tableLayoutPanel1.SetRowSpan(this.namespacePrefixesView, 2);
-            this.namespacePrefixesView.RowTemplate.Height = 28;
-            this.namespacePrefixesView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.namespacePrefixesView.Size = new System.Drawing.Size(476, 525);
-            this.namespacePrefixesView.TabIndex = 6;
-            this.namespacePrefixesView.CellValidating += new System.Windows.Forms.DataGridViewCellValidatingEventHandler(this.namespacePrefixesView_CellValidating);
-            // 
             // addDataNamespaceToMappingsButton
             // 
             this.addDataNamespaceToMappingsButton.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
@@ -204,6 +179,45 @@
             this.addDataNamespaceToMappingsButton.UseVisualStyleBackColor = true;
             this.addDataNamespaceToMappingsButton.Click += new System.EventHandler(this.addDataNamespaceToMappingsButton_Click);
             // 
+            // namespacePrefixesView
+            // 
+            this.namespacePrefixesView.AllowUserToAddRows = false;
+            this.namespacePrefixesView.AllowUserToDeleteRows = false;
+            this.namespacePrefixesView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.namespacePrefixesView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.namespacePrefixesView.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.namespacePrefixesView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.namespacePrefixesView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.prefix,
+            this.nspace});
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.namespacePrefixesView.DefaultCellStyle = dataGridViewCellStyle2;
+            this.namespacePrefixesView.Location = new System.Drawing.Point(172, 80);
+            this.namespacePrefixesView.MultiSelect = false;
+            this.namespacePrefixesView.Name = "namespacePrefixesView";
+            this.namespacePrefixesView.RowHeadersVisible = false;
+            this.tableLayoutPanel1.SetRowSpan(this.namespacePrefixesView, 2);
+            this.namespacePrefixesView.RowTemplate.Height = 28;
+            this.namespacePrefixesView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.namespacePrefixesView.Size = new System.Drawing.Size(476, 525);
+            this.namespacePrefixesView.TabIndex = 6;
+            this.namespacePrefixesView.CellValidating += new System.Windows.Forms.DataGridViewCellValidatingEventHandler(this.namespacePrefixesView_CellValidating);
+            // 
             // prefix
             // 
             this.prefix.FillWeight = 40F;
@@ -214,6 +228,10 @@
             // 
             this.nspace.HeaderText = "Namespace";
             this.nspace.Name = "nspace";
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
             // 
             // ExportOptionsForm
             // 
@@ -230,8 +248,8 @@
             this.Load += new System.EventHandler(this.ExportOptionsForm_Load);
             this.tableLayoutPanel1.ResumeLayout(false);
             this.tableLayoutPanel1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.namespacePrefixesView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
